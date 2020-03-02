8bc733b4281175898c62772f9d3091e8c5cadcf3
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2020-03-02 15:59:31 -0400

    Add files via upload

--- js/actions.js
+++ js/actions.js
@@ -4,3 +4,12 @@ for(var i=0;i<pos.length;i++){
     $(".pos").eq(i).html(i);
 }
 $(".pessoa .equipe").first().addClass("celestial");
+
+
+$(".pessoa").hover(function () {
+        $(this).css("box-shadow","inset 0px -3px "+$(this).find(".equipe").css("background-color"));
+    },
+    function(){
+        $(this).css("box-shadow","");
+    }
+);
